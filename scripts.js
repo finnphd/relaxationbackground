97c85a736a86a58946ede6600366514de7489283
Mike Finn <63814751+finnphd@users.noreply.github.com>
2023-10-26 14:33:20 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,33 +1,35 @@
-let mainAudio = document.getElementById("mainAudio");
-let backgroundAudio = document.getElementById("backgroundAudio");
-let isBackgroundPlaying = false;
+document.addEventListener('DOMContentLoaded', function() {
+    let mainAudio = document.getElementById("mainAudio");
+    let backgroundAudio = document.getElementById("backgroundAudio");
+    let isBackgroundPlaying = false;
 
-function playMain() {
-    mainAudio.play();
-}
-
-function stopMain() {
-    mainAudio.pause();
-    mainAudio.currentTime = 0;
-}
-
-function toggleBackground() {
-    if (isBackgroundPlaying) {
-        backgroundAudio.pause();
-    } else {
-        backgroundAudio.play();
+    function playMain() {
+        mainAudio.play();
     }
-    isBackgroundPlaying = !isBackgroundPlaying;
-}
 
-document.body.addEventListener("keydown", function (event) {
-    if (event.code === "Space") {
-        playMain();
+    function stopMain() {
+        mainAudio.pause();
+        mainAudio.currentTime = 0;
     }
-});
 
-document.body.addEventListener("keyup", function (event) {
-    if (event.code === "Space") {
-        stopMain();
+    function toggleBackground() {
+        if (isBackgroundPlaying) {
+            backgroundAudio.pause();
+        } else {
+            backgroundAudio.play();
+        }
+        isBackgroundPlaying = !isBackgroundPlaying;
     }
+
+    document.body.addEventListener("keydown", function (event) {
+        if (event.code === "Space") {
+            playMain();
+        }
+    });
+
+    document.body.addEventListener("keyup", function (event) {
+        if (event.code === "Space") {
+            stopMain();
+        }
+    });
 });
