822afe3c0d7cfd80cab339ea70dfbd2ca5bab67f
Mike Finn <63814751+finnphd@users.noreply.github.com>
2023-10-26 20:37:38 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,60 +1,65 @@
-// Create audio context
-var AudioContext = window.AudioContext || window.webkitAudioContext;
-var audioContext = new AudioContext();
+let foregroundAudio = new Audio("Responsive Hypnosis Example.mp3");
+let backgroundAudio = new Audio("background.mp3");
+let playButton = document.getElementById('play');
+let backgroundPlayButton = document.getElementById('backgroundPlayButton');
+let volumeControls = document.getElementById('volumeControls');
+let toggleVolumeButton = document.getElementById('toggleVolume');
+let foregroundVolumeSlider = document.getElementById('foregroundVolume');
+let backgroundVolumeSlider = document.getElementById('backgroundVolume');
 
-// Create AudioBufferSourceNode for both audios
-var sourceNodeMain = null;
-var sourceNodeBackground = null;
+foregroundAudio.volume = foregroundVolumeSlider.value;
+backgroundAudio.volume = backgroundVolumeSlider.value;
 
-// Load audio files into buffers
-function loadAudio(url, callback) {
-    var request = new XMLHttpRequest();
-    request.open('GET', url, true);
-    request.responseType = 'arraybuffer';
-    request.onload = function() {
-        audioContext.decodeAudioData(request.response, function(buffer) {
-            callback(buffer);
-        });
-    };
-    request.send();
-}
-
-var mainBuffer = null;
-loadAudio('Responsive Hypnosis Example.mp3', function(buffer) {
-    mainBuffer = buffer;
+foregroundVolumeSlider.addEventListener('input', function() {
+    foregroundAudio.volume = this.value;
 });
 
-var backgroundBuffer = null;
-loadAudio('background.mp3', function(buffer) {
-    backgroundBuffer = buffer;
+backgroundVolumeSlider.addEventListener('input', function() {
+    backgroundAudio.volume = this.value;
 });
 
-// Play function
-function playAudio(buffer, loop = false) {
-    var source = audioContext.createBufferSource();
-    source.buffer = buffer;
-    source.loop = loop;
-    source.connect(audioContext.destination);
-    source.start(0);
-    return source;
-}
-
-document.getElementById("mainCircle").addEventListener("click", function() {
-    // Play main audio on click
-    if (sourceNodeMain) {
-        sourceNodeMain.stop();
-        sourceNodeMain = null;
+toggleVolumeButton.addEventListener('click', function() {
+    if (volumeControls.style.right === "-350px") {
+        volumeControls.style.right = "0";
     } else {
-        sourceNodeMain = playAudio(mainBuffer);
+        volumeControls.style.right = "-350px";
     }
 });
 
-document.getElementById("bottomMiddleButton").addEventListener("click", function() {
-    // Play or pause background audio
-    if (sourceNodeBackground) {
-        sourceNodeBackground.stop();
-        sourceNodeBackground = null;
-    } else {
-        sourceNodeBackground = playAudio(backgroundBuffer, true);
+playButton.addEventListener('mousedown', startForegroundAudio);
+playButton.addEventListener('mouseup', stopForegroundAudio);
+playButton.addEventListener('touchstart', startForegroundAudio);
+playButton.addEventListener('touchend', stopForegroundAudio);
+playButton.addEventListener('keydown', function(event) {
+    if (event.key === ' ' || event.key === 'Spacebar') {
+        startForegroundAudio(event);
     }
 });
+playButton.addEventListener('keyup', function(event) {
+    if (event.key === ' ' || event.key === 'Spacebar') {
+        stopForegroundAudio(event);
+    }
+});
+
+backgroundPlayButton.addEventListener('click', toggleBackgroundAudio);
+
+function startForegroundAudio(e) {
+    e.preventDefault();
+    foregroundAudio.play();
+    playButton.style.transform = "scale(0.9)";
+}
+
+function stopForegroundAudio() {
+    foregroundAudio.pause();
+    playButton.style.transform = "scale(1)";
+}
+
+function toggleBackgroundAudio() {
+    if (backgroundAudio.paused) {
+        backgroundAudio.play();
+        backgroundPlayButton.textContent = "□";
+    } else {
+        backgroundAudio.pause();
+        backgroundPlayButton.textContent = "▶";
+    }
+}
